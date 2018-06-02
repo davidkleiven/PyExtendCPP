fddb1e7fcd61f0023149f092bb2a62563fdeefa3
David Kleiven <davidkleiven446@gmail.com>
2018-06-02 16:24:18 +0200

    Add Numpy To include dir

--- testmodule/setup.py
+++ testmodule/setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, Extension
+import numpy as np
 
 module = Extension("testmodule_cpp", sources=["testmodule.cpp","testlist.cpp"], \
-language="c++")
+language="c++", include_dirs=[np.get_include()])
 
 setup(
     name = "pyextend_testmodule",
